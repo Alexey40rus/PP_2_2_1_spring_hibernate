f9ac2ce87560491954a1bffdf7ad3c254f040622
Alexey40rus <mareich600@gmail.com>
2022-11-15 17:32:11 +0300

    Имена согласно конвенции

--- 2.2.1. spring_hibernate/src/main/java/hibernate/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hibernate/dao/UserDaoImp.java
@@ -5,12 +5,14 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
    private final SessionFactory sessionFactory;
+
    @Autowired
    public UserDaoImp(SessionFactory sessionFactory) {
       this.sessionFactory = sessionFactory;
@@ -23,13 +25,14 @@ public class UserDaoImp implements UserDao {
 
    @Override
    public List<User> listUsers() {
-      return sessionFactory.getCurrentSession().createQuery("from User").getResultList();
+      Query<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
    }
 
    @Override
    public User getUserByCar(String model, int series) {
-      String HQL = "FROM User user WHERE user.car.model=:model AND user.car.series=:series";
-      Query<User> query = sessionFactory.getCurrentSession().createQuery(HQL);
+      String hql = "from User user where user.car.model=:model and user.car.series=:series";
+      Query<User> query = sessionFactory.getCurrentSession().createQuery(hql);
       query.setParameter("model", model);
       query.setParameter("series", series);
       return query.getSingleResult();
